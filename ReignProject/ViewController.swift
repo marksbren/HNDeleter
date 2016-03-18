134add0e33d3732491c3e2b9a94d9d0d46e57146
Mark Brenneman <marksbren@gmail.com>
2016-03-18 10:57:24 -0700

    Initial Commit

---
+++ ReignProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReignProject
+//
+//  Created by Mark Brenneman on 3/18/16.
+//  Copyright © 2016 Mark Brenneman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
